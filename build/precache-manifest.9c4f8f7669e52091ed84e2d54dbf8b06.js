00f4cd52f7ae29063e60b9132980fdd735c41825
2skee <anbuwoods@gmail.com>
2021-02-07 11:01:34 +0200

    add express server for heroku deployment

---
+++ build/precache-manifest.9c4f8f7669e52091ed84e2d54dbf8b06.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f17353173f97b4f9d50317f2d48fac6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fe5e32bde5cf5748d92a",
+    "url": "/static/css/main.eac1cbba.chunk.css"
+  },
+  {
+    "revision": "3abee813f026faf6bd2b",
+    "url": "/static/js/2.e5954318.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/static/js/2.e5954318.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe5e32bde5cf5748d92a",
+    "url": "/static/js/main.06ae7516.chunk.js"
+  },
+  {
+    "revision": "f435b5232b5edb53200c",
+    "url": "/static/js/runtime-main.ae04ce71.js"
+  },
+  {
+    "revision": "cf801df0331b44534bd69df43be4837f",
+    "url": "/static/media/bwf.cf801df0.png"
+  },
+  {
+    "revision": "629051aaaba85cffdc2e6d66e3ea39ca",
+    "url": "/static/media/cart.629051aa.svg"
+  },
+  {
+    "revision": "4617c6a99bcf1536bf5254b6eb7f1217",
+    "url": "/static/media/ccbf.4617c6a9.png"
+  },
+  {
+    "revision": "b2b13fb5fde144c43346e9fcc51b5861",
+    "url": "/static/media/ccf.b2b13fb5.png"
+  },
+  {
+    "revision": "fa7e1ca71bf6b72e75ab1cef3e0e2c30",
+    "url": "/static/media/gwsf.fa7e1ca7.png"
+  },
+  {
+    "revision": "d6100b0ec7904e2e087b00c8f9ea3f5f",
+    "url": "/static/media/hff.d6100b0e.png"
+  },
+  {
+    "revision": "c1999a13cec3f2b608a37e622257a9e9",
+    "url": "/static/media/logo.c1999a13.png"
+  },
+  {
+    "revision": "502bb83d66ceb7f9cc7bca283a2b127d",
+    "url": "/static/media/vf.502bb83d.png"
+  },
+  {
+    "revision": "b05dc423340ff2eb3ae986a371494af5",
+    "url": "/static/media/ywbf.b05dc423.png"
+  },
+  {
+    "revision": "f5309313a8bbd4e4791cf943fd88b3c9",
+    "url": "/static/media/ywf.f5309313.png"
+  }
+]);
